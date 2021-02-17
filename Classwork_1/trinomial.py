312d7e586bcbc43b9fa74bf934fee45befd356a1
Ruben Petrosyan <petrosyanruben@gmail.com>
2021-02-17 20:01:59 +0400

    Working code

---
+++ Classwork_1/trinomial.py
@@ -0,0 +1,8 @@
+a = int(input('Enter a: '))
+b = int(input('Enter b: '))
+c = int(input('Enter c: '))
+x0 = int(input('Enter x0: '))
+
+ans = a * x0 ** 2 + b * x0  +  c
+
+print(f'\nThe answer is: {ans}')
