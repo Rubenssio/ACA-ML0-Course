d1d0f3e631df7c72bc5cd5e80295368ea79cba2c
Ruben Petrosyan <petrosyanruben@gmail.com>
2021-02-18 16:15:32 +0400

    Initial working code

---
+++ Homework_1/area_of_a_right_triangle.py
@@ -0,0 +1,6 @@
+print('CALCULATING THE AREA OF A RIGHT TRIANGLE')
+
+a = float(input('Enter the length of the first leg: '))
+b = float(input('Enter the length of the second leg: '))
+
+print('The area is:', a * b / 2)
