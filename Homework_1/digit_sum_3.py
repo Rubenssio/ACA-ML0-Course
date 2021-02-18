ca50c3db9541019a78d75c569ca3211d111b5fde
Ruben Petrosyan <petrosyanruben@gmail.com>
2021-02-18 16:08:48 +0400

    Initial working code

---
+++ Homework_1/digit_sum_3.py
@@ -0,0 +1,12 @@
+num = int(input('Enter the number: '))
+
+a = num % 10
+b = num % 100 // 10
+c = num // 100
+
+print('The sum of the digits is:', a + b + c)
+
+# Another solution
+# num = input('Enter the number: ')
+#
+# print('The sum of the digits is:', int(num[0]) + int(num[1]) + int(num[2]))
