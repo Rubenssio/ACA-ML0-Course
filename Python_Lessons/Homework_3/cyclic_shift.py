deb50078729e9749c6578530b8c0afe7fe8e0fc3
Ruben Petrosyan <petrosyanruben@gmail.com>
2021-04-08 15:41:36 +0400

    Regrouping folders

--- Python_Lessons/Homework_3/cyclic_shift.py
+++ Python_Lessons/Homework_3/cyclic_shift.py
@@ -2,8 +2,10 @@ N, k = [int(n) for n in input().split()]
 
 S = [int(n) for n in input().split()]
 
-for _ in range(k):
-    S = S[-1:] + S[:-1]
+for _ in range(k//N):
+    S = S[-1:].extend(S[:-1])
 
 for num in S:
     print(num, end=' ')
+
+print(1000%3)
